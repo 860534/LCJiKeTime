9371f4824863414852bef0baa2b63fe70a98e05c
850534 <2142686053nd@gmail.com>
2022-07-07 20:16:09 +0800

    day12

---
+++ src/main/java/com/kingdomai/SanZhouGongKeJinJie/Day12_1.java
@@ -0,0 +1,13 @@
+package com.kingdomai.SanZhouGongKeJinJie;
+
+public class Day12_1 {
+
+    public ListNode swapPairs(ListNode head) {
+        ListNode newHead = head;
+        while (head.next.next != null) {
+
+        }
+
+        return null;
+    }
+}
